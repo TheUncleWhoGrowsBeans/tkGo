288a48f0fe706e7b7d8167f606e281acdbe78bc6
Uncle Bean <413083153@qq.com>
2020-01-19 17:41:15 +0800

    feat(FrameTextMain): 设置行列权重

--- src/frame/frame_text_main.py
+++ src/frame/frame_text_main.py
@@ -4,20 +4,23 @@
 # @Author              : Uncle Bean
 # @Date                : 2020-01-13 17:35:10
 # @LastEditors: Uncle Bean
-# @LastEditTime: 2020-01-13 17:58:10
+# @LastEditTime: 2020-01-19 14:06:53
 # @FilePath            : \src\frame\frame_text_main.py
 # @Description         : 
 
 from tkinter import NSEW
-from tkinter.ttk import LabelFrame
+from frame.label_frame import ELabelFrame
 from text.text_main import TextMain
 
 
-class FrameTextMain(LabelFrame):
+class FrameTextMain(ELabelFrame):
 
     def __init__(self, master=None, **kw):
         
         super().__init__(master=master, **kw)
 
-        self.text_main = TextMain(master=self, width=110, height=35)
+        self.columnconfigure(0, weight=1)
+        
+        self.rowconfigure(0, weight=600)
+        self.text_main = TextMain(master=self)
         self.text_main.grid(row=0, column=0, sticky=NSEW)
