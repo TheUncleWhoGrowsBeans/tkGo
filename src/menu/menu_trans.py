60598727e181ed466ad8743d58ac113ffabf45cf
Uncle Bean <413083153@qq.com>
2020-01-19 17:39:03 +0800

    perf(img_to_excel): 修改重要输出信息形式为msgbox

--- src/menu/menu_trans.py
+++ src/menu/menu_trans.py
@@ -4,7 +4,7 @@
 # @Author              : Uncle Bean
 # @Date                : 2020-01-17 16:57:55
 # @LastEditors: Uncle Bean
-# @LastEditTime: 2020-01-18 20:48:00
+# @LastEditTime: 2020-01-19 14:32:54
 # @FilePath            : \src\menu\menu_trans.py
 # @Description         : 
 
@@ -29,7 +29,7 @@ class MenuTrans(EMenu):
     def img_to_excel(self):
         data_type, data_content = Clip.get_data()
         if data_type != Clip.DATA_TYPE_FILE:
-            self.stderr("请先复制Excel文件", title="错误")
+            self.msg_box_err("请先复制Excel文件", title="错误")
             return
             
         error_info = list()
@@ -47,9 +47,10 @@ class MenuTrans(EMenu):
             if len(excel.img_add_failed) > 0:
                 error_info.append((file, excel.img_add_failed, "add_failed"))
         
-        self.stdout("Finish")
         if len(error_info) > 0:
             for error in error_info:
-                self.stdout(error[0], error[2])
+                self.stderr(error[0], error[2])
                 for key, value in error[1].items():
-                    self.stdout(key, value)
+                    self.stderr(key, value)
+        
+        self.msg_box_info(self.LABEL_IMG_TO_EXCEL, "Finish")
