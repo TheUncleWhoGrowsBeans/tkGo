7c87a312a7d8252f7d9a0234a125c6f5dbdfd993
Uncle Bean <413083153@qq.com>
2020-01-19 17:41:54 +0800

    pref(FrameButtonMain): 修改为继承EFrame

--- src/frame/frame_button_main.py
+++ src/frame/frame_button_main.py
@@ -4,15 +4,16 @@
 # @Author              : Uncle Bean
 # @Date                : 2020-01-13 17:50:58
 # @LastEditors: Uncle Bean
-# @LastEditTime: 2020-01-13 22:52:23
+# @LastEditTime: 2020-01-19 14:07:29
 # @FilePath            : \src\frame\frame_button_main.py
 # @Description         : 
 
-from tkinter import Frame, Button
+from tkinter import Button
 from tkinter import NSEW
+from frame.frame import EFrame
 
 
-class FrameButtonMain(Frame):
+class FrameButtonMain(EFrame):
 
     def __init__(self, master=None, cnf={}, **kw):
         
