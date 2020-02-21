3d98044209f25861da09567beac02b4d8672bfb2
Uncle Bean <413083153@qq.com>
2020-02-21 22:50:53 +0800

    fix(ssh.py): 修复永远自动连接的bug

--- src/utils/remote/ssh.py
+++ src/utils/remote/ssh.py
@@ -4,12 +4,13 @@
 # @Author              : Uncle Bean
 # @Date                : 2020-02-18 09:16:23
 # @LastEditors: Uncle Bean
-# @LastEditTime: 2020-02-19 18:09:37
+# @LastEditTime: 2020-02-20 23:01:26
 # @FilePath            : \src\utils\remote\ssh.py
 # @Description         : 
 
 import logging
 import paramiko
+import traceback
 
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s[%(levelname)s]: %(message)s')
@@ -32,7 +33,7 @@ class SSH(object):
         self.transport = paramiko.Transport((
             self.host, 
             self.port
-        ))
+        )) if auto_connect else None
         self.stdin = None
         self.stdout = None
         self.stderr = None
@@ -41,17 +42,21 @@ class SSH(object):
         if auto_connect: self.transport_connect()
 
     def transport_connect(self):
-        if self.pkey:
-            pkey = paramiko.RSAKey.from_private_key_file(self.pkey)
-            self.transport.connect(
-                username=self.username, 
-                pkey=pkey
-                )
-        else:
-            self.transport.connect(
-                username=self.username, 
-                password=self.password
-                )
+        try:
+            if self.pkey:
+                pkey = paramiko.RSAKey.from_private_key_file(self.pkey)
+                self.transport.connect(
+                    username=self.username, 
+                    pkey=pkey
+                    )
+            else:
+                self.transport.connect(
+                    username=self.username, 
+                    password=self.password
+                    )
+        except Exception as e:
+            traceback.print_exc()
+            self.stderr(str(e))
     
     def close(self):
         self.transport.close()
