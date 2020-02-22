b7f72562a2374a33822b33dc163bc63e9dd0e081
Uncle Bean <413083153@qq.com>
2020-02-22 20:29:52 +0800

    fix(ssh.py): 修复自动连接问题

--- src/utils/remote/ssh.py
+++ src/utils/remote/ssh.py
@@ -4,7 +4,7 @@
 # @Author              : Uncle Bean
 # @Date                : 2020-02-18 09:16:23
 # @LastEditors: Uncle Bean
-# @LastEditTime: 2020-02-20 23:01:26
+# @LastEditTime: 2020-02-21 23:16:41
 # @FilePath            : \src\utils\remote\ssh.py
 # @Description         : 
 
@@ -23,26 +23,32 @@ class SSH(object):
             username, 
             password=None, 
             pkey=None,
-            auto_connect=True
+            auto_connect=True,
+            stdout=print,
+            stderr=print
             ):
         self.host = host
         self.port = port
         self.username = username
         self.password = password
         self.pkey = pkey
-        self.transport = paramiko.Transport((
-            self.host, 
-            self.port
-        )) if auto_connect else None
-        self.stdin = None
-        self.stdout = None
-        self.stderr = None
+        self.transport = None
+        self.stdout = stdout
+        self.stderr = stderr
+        
+        self.cmd_stdin = None
+        self.cmd_stdout = None
+        self.cmd_stderr = None
         self.sftp = None
         self.ssh = None
         if auto_connect: self.transport_connect()
 
     def transport_connect(self):
         try:
+            self.transport = paramiko.Transport((
+                self.host, 
+                self.port
+            ))
             if self.pkey:
                 pkey = paramiko.RSAKey.from_private_key_file(self.pkey)
                 self.transport.connect(
@@ -74,11 +80,11 @@ class SSH(object):
 
     def exec_command(self, cmd):
         if not self.ssh: self.get_ssh()
-        self.stdin, self.stdout, self.stderr = self.ssh.exec_command(cmd)
-        return self.stdout.channel.recv_exit_status()
+        self.cmd_stdin, self.cmd_stdout, self.cmd_stderr = self.ssh.exec_command(cmd)
+        return self.cmd_stdout.channel.recv_exit_status()
     
     def output(self, stdout=print, stderr=print):
-        for line in self.stdout.readlines():
+        for line in self.cmd_stdout.readlines():
             stdout(line)
-        for line in self.stderr.readlines():
+        for line in self.cmd_stderr.readlines():
             stderr(line)
